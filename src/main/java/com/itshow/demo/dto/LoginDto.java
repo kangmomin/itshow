28f45b5b209537e5deb67fe6e7ace885a7a376c5
koldin108902 <alex108902@naver.com>
2023-05-24 04:14:04 +0900

    fixed: 빈 생성자 등록

--- src/main/java/com/itshow/demo/dto/LoginDto.java
+++ src/main/java/com/itshow/demo/dto/LoginDto.java
@@ -1,11 +1,14 @@
 package com.itshow.demo.dto;
 
 import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class LoginDto {
 
     @Size(min = 5, max = 30)
