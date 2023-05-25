7c9f042218284311ac8c544f68a0fce0adc930b3
koldin108902 <alex108902@naver.com>
2023-05-26 04:20:52 +0900

    update: 없는 유저의 jwt가 들어오는 에러 핸들링

--- src/main/java/com/itshow/demo/config/security/Jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/itshow/demo/config/security/Jwt/JwtAuthenticationFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -22,9 +23,14 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         // 유효한 토큰인지 확인합니다.
         if (token != null && jwtTokenProvider.validateToken(token)) {
             // 토큰이 유효하면 토큰으로부터 유저 정보를 받아옵니다.
-            Authentication authentication = jwtTokenProvider.getAuthentication(token);
-            // SecurityContext 에 Authentication 객체를 저장합니다.
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+            try {
+                Authentication authentication = jwtTokenProvider.getAuthentication(token);
+                // SecurityContext 에 Authentication 객체를 저장합니다.
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } catch (UsernameNotFoundException e) {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "member is not exist");
+                return;
+            }
         }
         chain.doFilter(request, response);
     }
